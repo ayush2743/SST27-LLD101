99ee7ddbc0b2bacc7f5f385206e0722a3eb84814
ayush2743 <saxenaayush27.work@gmail.com>
2025-09-03 17:06:25 +0530

    employee-adapter-java-sources

---
+++ employee-adapter-java-sources/exercise/src/main/java/exercise/EmployeeLDAPAdapter.java
@@ -0,0 +1,33 @@
+package exercise;
+
+public class EmployeeLDAPAdapter implements Employee {
+    private final EmployeeLDAP employeeLDAP;
+    
+    public EmployeeLDAPAdapter(EmployeeLDAP employeeLDAP) {
+        this.employeeLDAP = employeeLDAP;
+    }
+    
+    @Override
+    public String getId() {
+        String uid = employeeLDAP.get("uid");
+        return uid != null ? uid : "";
+    }
+    
+    @Override
+    public String getFirstName() {
+        String givenName = employeeLDAP.get("givenName");
+        return givenName != null ? givenName : "";
+    }
+    
+    @Override
+    public String getLastName() {
+        String sn = employeeLDAP.get("sn");
+        return sn != null ? sn : "";
+    }
+    
+    @Override
+    public String getEmail() {
+        String mail = employeeLDAP.get("mail");
+        return mail != null ? mail : "";
+    }
+}
